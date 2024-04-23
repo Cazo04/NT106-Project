6b59e841be488455102b5eb8e70ddc176683269f
Cazo04 <nguyenkhang270604@gmail.com>
2024-04-23 17:40:25 +0700

    Add project files.

---
+++ NT106-Admin/Form1.cs
@@ -0,0 +1,10 @@
+namespace NT106_Admin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NT106-User/Main.cs
@@ -0,0 +1,10 @@
+namespace NT106_User
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
