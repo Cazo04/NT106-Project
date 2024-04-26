e483439fb36a8d54d72b0d1c66f007dc03a2641b
Cazo04 <nguyenkhang270604@gmail.com>
2024-04-26 08:12:23 +0700

    Update movie data entry

--- NT106-Admin/MoviesManager.Designer.cs
+++ NT106-Admin/MoviesManager.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
-            panel1 = new Panel();
-            panel2 = new Panel();
-            btnRefreshMovies = new Button();
             cMovieID = new DataGridViewTextBoxColumn();
             cMovieName = new DataGridViewTextBoxColumn();
             cReleaseDate = new DataGridViewTextBoxColumn();
@@ -41,14 +38,31 @@
             cIsTVShows = new DataGridViewCheckBoxColumn();
             cView = new DataGridViewButtonColumn();
             cDel = new DataGridViewButtonColumn();
+            panel1 = new Panel();
             panel3 = new Panel();
+            panel2 = new Panel();
+            btnRefreshMovies = new Button();
             panel4 = new Panel();
             panel5 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
             panel3.SuspendLayout();
+            panel2.SuspendLayout();
             panel4.SuspendLayout();
+            panel5.SuspendLayout();
             SuspendLayout();
             // 
             // dataGridView1
@@ -63,35 +77,6 @@
             dataGridView1.ReadOnly = true;
             dataGridView1.Size = new Size(450, 561);
             dataGridView1.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(panel3);
-            panel1.Controls.Add(panel2);
-            panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(450, 598);
-            panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            panel2.Controls.Add(btnRefreshMovies);
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(0, 0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(450, 37);
-            panel2.TabIndex = 2;
-            // 
-            // btnRefreshMovies
-            // 
-            btnRefreshMovies.Dock = DockStyle.Left;
-            btnRefreshMovies.Location = new Point(0, 0);
-            btnRefreshMovies.Name = "btnRefreshMovies";
-            btnRefreshMovies.Size = new Size(99, 37);
-            btnRefreshMovies.TabIndex = 0;
-            btnRefreshMovies.Text = "Refresh";
-            btnRefreshMovies.UseVisualStyleBackColor = true;
             // 
             // cMovieID
             // 
@@ -147,6 +132,16 @@
             cDel.Name = "cDel";
             cDel.ReadOnly = true;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(450, 598);
+            panel1.TabIndex = 1;
+            // 
             // panel3
             // 
             panel3.Controls.Add(dataGridView1);
@@ -155,6 +150,25 @@
             panel3.Name = "panel3";
             panel3.Size = new Size(450, 561);
             panel3.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btnRefreshMovies);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(450, 37);
+            panel2.TabIndex = 2;
+            // 
+            // btnRefreshMovies
+            // 
+            btnRefreshMovies.Dock = DockStyle.Left;
+            btnRefreshMovies.Location = new Point(0, 0);
+            btnRefreshMovies.Name = "btnRefreshMovies";
+            btnRefreshMovies.Size = new Size(99, 37);
+            btnRefreshMovies.TabIndex = 0;
+            btnRefreshMovies.Text = "Refresh";
+            btnRefreshMovies.UseVisualStyleBackColor = true;
             // 
             // panel4
             // 
@@ -169,11 +183,130 @@
             // 
             // panel5
             // 
+            panel5.Controls.Add(textBox6);
+            panel5.Controls.Add(textBox4);
+            panel5.Controls.Add(textBox2);
+            panel5.Controls.Add(label7);
+            panel5.Controls.Add(textBox5);
+            panel5.Controls.Add(label5);
+            panel5.Controls.Add(textBox3);
+            panel5.Controls.Add(label6);
+            panel5.Controls.Add(label3);
+            panel5.Controls.Add(label4);
+            panel5.Controls.Add(textBox1);
+            panel5.Controls.Add(label2);
+            panel5.Controls.Add(label1);
             panel5.Dock = DockStyle.Top;
             panel5.Location = new Point(5, 0);
             panel5.Name = "panel5";
             panel5.Size = new Size(382, 417);
             panel5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13F);
+            label1.Location = new Point(12, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Movie Data Entry";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Movie Name:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 68);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(254, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Movie Name:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 120);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(254, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Movie Name:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 179);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(254, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 213);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 15);
+            label5.TabIndex = 1;
+            label5.Text = "Movie Name:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 231);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(254, 23);
+            textBox4.TabIndex = 2;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 269);
+            label6.Name = "label6";
+            label6.Size = new Size(78, 15);
+            label6.TabIndex = 1;
+            label6.Text = "Movie Name:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 287);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(254, 23);
+            textBox5.TabIndex = 2;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 321);
+            label7.Name = "label7";
+            label7.Size = new Size(78, 15);
+            label7.TabIndex = 1;
+            label7.Text = "Movie Name:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(12, 339);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(254, 23);
+            textBox6.TabIndex = 2;
             // 
             // MoviesManager
             // 
@@ -187,9 +320,11 @@
             Text = "MoviesManager";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             panel1.ResumeLayout(false);
-            panel2.ResumeLayout(false);
             panel3.ResumeLayout(false);
+            panel2.ResumeLayout(false);
             panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -211,5 +346,18 @@
         private Panel panel3;
         private Panel panel4;
         private Panel panel5;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox6;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private Label label7;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
     }
 }
